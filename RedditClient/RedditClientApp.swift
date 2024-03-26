dd083865b13c1533daa07863dc2578da67da54ea
Mike Gonzalez <mikegb.dev@gmail.com>
2024-03-26 15:26:11 -0500

    Initial Commit

---
+++ RedditClient/RedditClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditClientApp.swift
+//  RedditClient
+//
+//  Created by Miguel Gonzalez on 3/26/24.
+//
+
+import SwiftUI
+
+@main
+struct RedditClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
