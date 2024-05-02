b6c1b99708e55f63fb9b7e44f375b7d80ef8c59c
Mike Gonzalez <mikegb.dev@gmail.com>
2024-05-02 17:36:23 -0500

    Added unit tests for `RedditPostsRepository`

---
+++ RedditClientTests/Utilities/Publisher+Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Publisher+Extensions.swift
+//  RedditClientTests
+//
+//  Created by Miguel Gonzalez on 5/1/24.
+//
+
+import Combine
+
+extension Publisher {
+    func flexValue(with valueReceiver: @escaping (Output) -> Void) {
+        let flex = FlexValueSubscriber<Output, Failure>(valueReceiver: valueReceiver)
+        subscribe(flex)
+    }
+}
+
+struct FlexValueSubscriber<Input, Failure: Error>: Subscriber {
+    let valueReceiver: (Input) -> Void
+    let completionReceiver: ((Subscribers.Completion<Failure>) -> Void)? = nil
+    let combineIdentifier = CombineIdentifier()
+    
+    func receive(subscription: any Subscription) {
+        subscription.request(.unlimited)
+    }
+    
+    func receive(_ input: Input) -> Subscribers.Demand {
+        valueReceiver(input)
+        return .unlimited
+    }
+    
+    func receive(completion: Subscribers.Completion<Failure>) {
+        completionReceiver?(completion)
+    }
+}
